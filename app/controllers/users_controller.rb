c793529e2c13e1b6302d25bedc692985f9f3bbbb
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-05 14:57:51 -0600

    generated user resource and added secure password requirement to user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def signup
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'users/signup'
+  get 'users/edit'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
