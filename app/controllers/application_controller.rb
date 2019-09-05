5a7a02106b5507ecf6ed47b4ea1d9f524a5ca60b
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-05 16:08:34 -0600

    started building basic user auth in users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+   #this might need to go into a helper actuall - REFACTOR
+   def display_errors(object)
+      object.errors.full_messages.join("\n")
+   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,20 @@ class UsersController < ApplicationController
   
   # this is the new route 
   def signup
-  
+    @user = User.new
   end
 
   def create
-  
+    @user = User.create(user_params)
+
+    if @user.id
+      flash[:notify] = "Account Successfully Created.\nYou have also been logged in."
+      session[:user_id] = @user.id
+      redirect_to locations_path
+    else
+      flash[:notify] = "Errors detected!\n#{display_errors(@user)}"
+      render :signup
+    end
   end
 
   def show
