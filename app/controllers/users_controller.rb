058bd80c93e84008699229a1b48e3acc535a39d7
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-05 15:05:49 -0600

    added additional restful methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  
+  # this is the new route 
   def signup
+  
+  end
+
+  def create
+  
+  end
+
+  def show
   end
 
   def edit
   end
 
-  def show
+  #Also need to add the facebook omniauth route etc.  
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :uid)
   end
+  
 end
