1677f13785da37f6229bdb7ee348bf7d8806a709
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-06 11:30:59 -0600

    Login form working and redirecting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
   # aka "new"
   def login
+    @user = User.new
   end
 
   def create
-  
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user
+      authenticated = @user.authenticate(params[:user][:password])
+
+      if authenticated
+        session[:user_id] = @user.id
+        flash[:notify] = "Successfully Logged In!"
+        redirect_to locations_path
+      else
+        flash[:notify] = "Incorrect Password"
+        redirect_to login_path
+      end
+    else
+      flash[:notify] = "Email not found"
+      redirect_to login_path
+    end
+
   end
 
   #aka destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get '/signup', to: "users#signup"
   get '/login', to: "sessions#login"
+  post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#logout"
   
   resources :users, except: [:new, :delete]
