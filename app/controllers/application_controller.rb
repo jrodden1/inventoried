9956dc5a57b59e529e0082ef8038f2fe38354ee2
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-09 14:37:33 -0600

    making progress on locations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,22 @@ class ApplicationController < ActionController::Base
    def display_errors(object)
       object.errors.full_messages.join("\n")
    end
+
+   def current_user
+      @user = User.find_by_id(session[:user_id])
+   end
+   
+   def is_logged_in?
+      if !!session[:user_id] 
+         true
+      else 
+         flash[:notify] = "You must be logged in to view this resource"
+         redirect_to login_path
+      end
+   end
+
+   def authorized?(current_user_obj=@user, resource_user_id)
+     current_user_obj.id == resource_user_id
+   end 
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,26 @@
 class LocationsController < ApplicationController
+   before_action :is_logged_in?
+   before_action :current_user
 
    def index
+      @locations = @user.locations
    end
    
    def new
+      @location = Location.new
    end
    
    def create
+      @location = Location.new(location_params)
+      binding.pry
+      if authorized?(@user.id, @location.user_id)
+         @location = Location.find_by(:name)
+         flash[:notify] = "#{@location.name} successfully created!"
+         redirect_to location_path(@location)
+      else
+         flash[:notify] = "You are not authorized to perform this action"
+         redirect_to login_path
+      end
    end
    
    def show 
