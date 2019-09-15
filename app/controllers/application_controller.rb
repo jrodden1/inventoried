505b5b4db38e3c2d6bfe5bead65b35989b453f6f
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-14 21:23:38 -0600

    updated the display_errors application controller method to join by a comma

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
    #this might need to go into a helper actuall - REFACTOR
    def display_errors(object)
-      object.errors.full_messages.join("\n")
+      object.errors.full_messages.join(", ")
    end
 
    def current_user
