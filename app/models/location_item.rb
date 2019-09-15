8e39902cb0f6f3d12563d56a1b8da40d140ab85b
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-15 17:16:46 -0600

    added model methods to location_item for item_description and item_name to remove chaining from views.

--- app/models/location_item.rb
+++ app/models/location_item.rb
@@ -3,4 +3,12 @@ class LocationItem < ApplicationRecord
    belongs_to :item
 
    validates :quantity, presence: true
+
+   def item_name
+      self.item.name
+   end
+
+   def item_description
+      self.item.description
+   end
 end
