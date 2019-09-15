9ad168f563331b829327ab3cf54ce850dd0580a1
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-15 09:06:23 -0600

    added comment to static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def home
+    #if the user is already logged in, when they try to go to the homepage they will get redirected to their locations path
     if !!session[:user_id]
       redirect_to locations_path
     end
