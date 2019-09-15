6f8bc39f8a62d7c5ee6a6a7c5107b3fecce5d60f
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-15 09:04:18 -0600

    adding comments to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,14 @@ class User < ApplicationRecord
    validates :email, presence: true, uniqueness: true
 
    def self.find_or_create_by_facebook_omniauth(auth)
+      #see if I have an existing user to match up with first. 
       user = User.find_by(email: auth['info']['email'])
 
+      #if I don't find a user, then create one from scratch
       if user.nil?
          user = User.create(email: auth['info']['email'], name: auth['info']['name'], password: SecureRandom.hex, uid: auth['uid'])
       elsif user.uid.nil?
+      #if I did find an existing user with the same email, then just set the uid.
          user.update(uid: auth['uid'])
          user
       end
