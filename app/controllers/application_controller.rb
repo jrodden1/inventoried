7293e7a18289590f7054e37cb1df3656719bcb90
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-15 09:22:25 -0600

    added comments to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-
-   #this might need to go into a helper actuall - REFACTOR
+   #displays errors the object fed to it. 
    def display_errors(object)
       object.errors.full_messages.join(", ")
    end
 
+   #gets the current user - used mainly by my controllers
    def current_user
       @user = User.find_by_id(session[:user_id])
    end
    
+   #checks to see if a user is logged in - used mainly by my controllers
    def is_logged_in?
       if !!session[:user_id] 
          true
@@ -18,10 +19,14 @@ class ApplicationController < ActionController::Base
       end
    end
 
+   #my authorization watchdog method - checks to make sure that the user that is trying to do an action is the one who owns that resource.
    def authorized?(current_user_id: @user.id, resource_user_id:, redirect_if_logged_in_path: locations_path)
+      #my legitimacy checker - are you the real sparticus? 
       if current_user_id == resource_user_id
+         #you are the real sparticus, do as you wish.
          yield if block_given?
       else
+         #Nay, you are an imposter!  you shall be redirected accordingly.
          flash[:alert] = "You are not authorized to perform this action"
 
          if is_logged_in? 
@@ -31,5 +36,4 @@ class ApplicationController < ActionController::Base
          end
       end
    end 
-
 end
