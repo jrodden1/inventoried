6997933b395f70d1646c95cbc81c7304c20d11d6
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-15 08:44:33 -0600

    cleaning up location_item model code, also removed unneeded link from location_items new view

--- app/models/location_item.rb
+++ app/models/location_item.rb
@@ -2,16 +2,5 @@ class LocationItem < ApplicationRecord
    belongs_to :location
    belongs_to :item
 
-   validate :no_dupe_items_in_a_location
-
    validates :quantity, presence: true
-
-   def no_dupe_items_in_a_location
-      !LocationItem.where("location_id = ? AND item_id = ?", self.location_id, self.item_id)
-   end
-   
-   # def quantity
-   #    self.quantity
-   # end
-   
 end
